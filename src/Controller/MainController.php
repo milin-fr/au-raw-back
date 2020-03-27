af959159b6036cd066791e693ce105b7e7d2a135
unknown <m.ilin.fr@gmail.com>
2020-03-27 10:25:07 +0100

    about us page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,12 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function home(ProductRepository $productRepository)
     {
-        return $this->render('home.html.twig', [
+        return $this->render('main\home.html.twig', [
             'products' => $productRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/about-us", name="about_us")
+     */
+    public function about_us()
+    {
+        return $this->render('main\about-us.html.twig', [
         ]);
     }
 }
