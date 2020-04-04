da2495d47e08bc2f06ff3f3154fbd7c83d9597d0
unknown <m.ilin.fr@gmail.com>
2020-04-04 12:22:04 +0200

    contact form validation

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -29,6 +29,9 @@ class Contact
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank(
+     *      message = "Please, provide your email, so we can contact you"
+     * )
      * @Assert\Length(
      *      max = 255
      * )
@@ -43,16 +46,21 @@ class Contact
      * )
      * @Assert\Regex(
      *  pattern="/[0-9]{10}/",
-     *  message="Please, make sure you provided 10 digits"
+     *  message="Please, make sure you provided 10 digits of your phone number"
      * )
      */
     private $phone_number;
 
     /**
      * @ORM\Column(type="text", nullable=true)
-     * @Assert\NotBlank
+     * @Assert\NotBlank(
+     *      message = "Please, provide more information about your request (at least 20 characters)"
+     * )
      * @Assert\Length(
      *      min = 20,
+     *      max = 3000,
+     *      minMessage = "Please, provide more information about your request (at least {{ limit }} characters)",
+     *      maxMessage = "Please, limit your massage to a maximum of {{ limit }} characters",
      *      allowEmptyString = false
      * )
      */
