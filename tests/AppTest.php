e63e41c09467425f7da7028a74691a1fd6e8f8a1
unknown <m.ilin.fr@gmail.com>
2020-05-17 16:44:43 +0200

    drafting a test

---
+++ tests/AppTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+    public function testTestsAreWorking () {
+        $this->assertEquals(2, 1+1);
+    }
+
+    //public function testTestNotWorking () {
+    //    $this->assertEquals(3, 1+1);
+    //}
+}
