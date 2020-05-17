9e19a0ca2fbbfac025f926e77c0d4e204af7f1d4
unknown <m.ilin.fr@gmail.com>
2020-05-17 17:47:28 +0200

    test for homepage

--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -6,8 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class ControlloerTest extends WebTestCase {
     public function testHomePage () {
         $client = static::createClient();
-        $client->request('GET', '');
-        $this->assertResponseStatusCodeSame(200);
+        $client->request('GET', '/');
+        $this->assertSelectorTextContains('.name', 'Strawberry Cheesecake');
+        // $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testAdminPage () {
+        $client = static::createClient();
+        $client->request('GET', '/admin');
+        $this->assertResponseRedirects();
     }
 
 }
