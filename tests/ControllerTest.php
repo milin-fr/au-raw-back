c0cadb7fca3cb9d96c1026b05d4ff53f8402f66c
unknown <m.ilin.fr@gmail.com>
2020-05-17 17:19:59 +0200

    setting up test db

--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -4,11 +4,10 @@ namespace App\Tests;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ControlloerTest extends WebTestCase {
-    public function testTestsAreWorking () {
-        $this->assertEquals(2, 1+1);
+    public function testHomePage () {
+        $client = static::createClient();
+        $client->request('GET', '');
+        $this->assertResponseStatusCodeSame(200);
     }
 
-    //public function testTestNotWorking () {
-    //    $this->assertEquals(3, 1+1);
-    //}
 }
